af73dc99e5b4e7d52e6c11a4c38ef9370515d940
Wahyu Taufik <wahyutaufik37@gmail.com>
2014-06-04 16:31:08 +0700

    First bono installation

---
+++ config/config.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * Bono App Configuration
+ *
+ * @category  PHP_Framework
+ * @package   Bono
+ * @author    Ganesha <reekoheek@gmail.com>
+ * @copyright 2013 PT Sagara Xinix Solusitama
+ * @license   https://raw.github.com/xinix-technology/bono/master/LICENSE MIT
+ * @version   0.10.0
+ * @link      http://xinix.co.id/products/bono
+ */
+
+use Norm\Schema\String;
+use Norm\Schema\Password;
+
+return array(
+    'app.about' => array(
+        'title' => 'Bono Application',
+    ),
+    'bono.salt' => 'please change this',
+    'bono.providers' => array(
+        '\\Norm\\Provider\\NormProvider' => array(
+            'datasources' => array(
+                'mongo' => array(
+                    'driver' => '\\Norm\\Connection\\MongoConnection',
+                    'database' => 'bono',
+                ),
+            ),
+            'collections' => array(
+                'mapping' => array(
+                    'User' => array(
+                        'schema' => array(
+                            'username' => String::create('username')->filter('trim|required|unique:User,username'),
+                            'password' => Password::create('password')->filter('trim|confirmed|salt'),
+                            'email' => String::create('email')->filter('trim|required|unique:User,email'),
+                            'first_name' => String::create('first_name')->filter('trim|required'),
+                            'last_name' => String::create('last_name')->filter('trim|required'),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+        '\\Xinix\\Migrate\\Provider\\MigrateProvider' => array(
+            // 'token' => 'changetokenherebeforeenable',
+        ),
+        '\\App\\Provider\\AppProvider',
+    ),
+    'bono.middlewares' => array(
+        '\\Bono\\Middleware\\ControllerMiddleware' => array(
+            'default' => '\\Norm\\Controller\\NormController',
+            'mapping' => array(
+                '/user' => null,
+            ),
+        ),
+        '\\Bono\\Middleware\\ContentNegotiatorMiddleware' => array(
+            'extensions' => array(
+                'json' => 'application/json',
+            ),
+            'views' => array(
+                'application/json' => '\\Bono\\View\\JsonView',
+            ),
+        ),
+        // uncomment below to enable auth
+        // '\\ROH\\BonoAuth\\Middleware\\AuthMiddleware' => array(
+        //     'driver' => '\\ROH\\BonoAuth\\Driver\\NormAuth',
+        // ),
+        '\\Bono\\Middleware\\NotificationMiddleware' => null,
+        '\\Bono\\Middleware\\SessionMiddleware' => null,
+    ),
+);
